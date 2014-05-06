fe3a2763e39c02b41cc5c4199b8d6c4a12608a6d
corot <jsantossimon@gmail.com>
2014-05-07 00:37:51 +0200

    Create an empty cpp lib

---
+++ world_canvas_client/include/world_canvas_client_cpp/world_canvas_client.h
@@ -0,0 +1,18 @@
+/*
+ * WorldCanvasClient.h
+ *
+ *  Created on: May 7, 2014
+ *      Author: jorge
+ */
+
+#ifndef WORLDCANVASCLIENT_H_
+#define WORLDCANVASCLIENT_H_
+
+class WorldCanvasClient
+{
+public:
+  WorldCanvasClient();
+  virtual ~WorldCanvasClient();
+};
+
+#endif /* WORLDCANVASCLIENT_H_ */
---
+++ world_canvas_client/src/world_canvas_client.cpp
@@ -0,0 +1,20 @@
+/*
+ * WorldCanvasClient.cpp
+ *
+ *  Created on: May 7, 2014
+ *      Author: jorge
+ */
+
+#include "world_canvas_client_cpp/world_canvas_client.h"
+
+WorldCanvasClient::WorldCanvasClient()
+{
+  // TODO Auto-generated constructor stub
+
+}
+
+WorldCanvasClient::~WorldCanvasClient()
+{
+  // TODO Auto-generated destructor stub
+}
+
